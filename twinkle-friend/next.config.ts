8e5e972804068745ebd61118b0d8ba799f808e97
Sminp <0102262@gmail.com>
2025-06-18 16:48:53 +0900

    fix: github actions 레포지토리 이름 변경

--- twinkle-friend/next.config.ts
+++ twinkle-friend/next.config.ts
@@ -7,8 +7,7 @@ const nextConfig: NextConfig = {
   // GitHub Pages URL에 저장소 이름이 포함된다면 반드시 설정해야 합니다.
   // 예: https://<username>.github.io/<repository-name>
   // 저장소 이름이 'my-nextjs-app' 이라면 '/my-nextjs-app'으로 설정
-  basePath:
-    process.env.NODE_ENV === "production" ? "/YOUR_REPOSITORY_NAME" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/twinkle-friend" : "",
 
   images: {
     unoptimized: true, // 정적 내보내기 시 Next.js Image 컴포넌트 최적화 비활성화
