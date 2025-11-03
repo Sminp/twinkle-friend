760a130f798d7c3b2dd4464cb4c1c575934449be
Sminp <0102262@gmail.com>
2025-11-03 23:43:28 +0900

    feat: 이미지 파일 생성 후 편하게 경로 넣기  파일 생성

---
+++ twinkle-friend/constants/image.ts
@@ -0,0 +1,7 @@
+// 이미지 경로를 보관합니다.
+
+export const Dog = "/images/common/dog.png";
+export const Cat = "/images/common/cat.png";
+export const Kakao = "/images/auth/kakao.svg";
+
+export const ArrowLeft = "/images/common/chevron-left.svg";
